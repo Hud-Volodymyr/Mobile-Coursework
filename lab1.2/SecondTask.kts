214cf6e1d0e5de1be45d33478d16821f2725ee2c
Hud-Volodymyr <v.gud2001@gmail.com>
2021-03-28 16:30:39 +0300

    Fix wrong behavior with negative and impossible coordinates

--- lab1.2/SecondTask.kts
+++ lab1.2/SecondTask.kts
@@ -14,10 +14,10 @@ class CoordinateVH(
             (degrees in -90..90 && direction === Direction.latitude) ||
             (degrees in -180..180 && direction === Direction.longitude)
     ) degrees else error("Degrees out of range: $degrees.\n Allowed values are between -180 and 180 for longitude and between -90 and 90 for latitude")
-    val minutes = if (minutes in 0u..59u) minutes
-        else error("Minutes out of range: $minutes.\n Allowed values are between 0 and 59")
-    val seconds = if (seconds in 0u..59u) seconds
-        else error("Seconds out of range: $seconds.\n Allowed values are between 0 and 59")
+    val minutes = if (minutes in 0u..59u && ((degrees !== 90 && degrees !== -90 && degrees !== 180  && degrees !== -180) || minutes == 0u)) minutes
+        else error("Minutes out of range: $minutes.\n Allowed values are between 0 and 59 for up to 90 degrees")
+    val seconds = if (seconds in 0u..59u && ((degrees !== 90 && degrees !== -90 && degrees !== 180  && degrees !== -180) || seconds == 0u)) seconds
+        else error("Seconds out of range: $seconds.\n Allowed values are between 0 and 59 for up to 90 degrees")
 
     val orientation = if (degrees <= 0 && direction == Direction.latitude) 'S'
         else if (direction == Direction.latitude) 'N'
@@ -25,11 +25,11 @@ class CoordinateVH(
         else 'E'
 
     fun stringify() : String {
-        return "$degrees°$minutes'$seconds\"$orientation\""
+        return "${degrees.absoluteValue}°$minutes'$seconds\"$orientation\""
     }
 
     fun decimalStringify() : String {
-        return "${((degrees.absoluteValue * 3600 + minutes.toInt() * 60 + seconds.toInt()) * degrees.sign).toFloat() / 3600}° $orientation)"
+        return "${((degrees.absoluteValue * 3600 + minutes.toInt() * 60 + seconds.toInt())).toFloat() / 3600}° $orientation)"
     }
 
     private fun signedSeconds() : Int {
@@ -82,6 +82,14 @@ fun main() {
     val avarageCoordinate = coordinate2.average(coordinate4)
     println("Avarage coordinate")
     println(avarageCoordinate?.stringify())
+
+    //val coordinateTooBig = CoordinateVH(90, 15u, 9u, Direction.latitude)
+
+    val coordinateNegative = CoordinateVH(-12, 30u, 40u, Direction.latitude)
+    println(coordinateNegative.stringify())
+    println(coordinateNegative.decimalStringify())
+
+
 }
 
 
